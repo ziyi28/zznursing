0d2518d6a285b59726ee06c65bbf6a4c69fb2b76
ychenchen <ycc@itcast.cn>
2025-05-17 10:55:20 +0800

    初始化后端项目

---
+++ zzyl-common/src/main/java/com/zzyl/common/exception/user/UserException.java
@@ -0,0 +1,18 @@
+package com.zzyl.common.exception.user;
+
+import com.zzyl.common.exception.base.BaseException;
+
+/**
+ * 用户信息异常类
+ * 
+ * @author ruoyi
+ */
+public class UserException extends BaseException
+{
+    private static final long serialVersionUID = 1L;
+
+    public UserException(String code, Object[] args)
+    {
+        super("user", code, args, null);
+    }
+}
